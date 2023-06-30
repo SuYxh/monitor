f6706cb677cafb9bbd4ab2fc1b81d5ad40e0f36b
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-06-30 23:20:17 +0800

    捕获promise错误

--- src/lib/jsError.js
+++ src/lib/jsError.js
@@ -23,4 +23,41 @@ export function injectJsError() {
             selector: lastEvent ? getSelector(lastEvent.path) : '' //代表最后一个操作的元素
         });
     }, true);
+
+    // 坚挺 promise 错误
+    window.addEventListener('unhandledrejection', (event) => {
+        console.log(event);
+        let lastEvent = getLastEvent();//最后一个交互事件
+        let message;
+        let filename;
+        let line = 0;
+        let column = 0;
+        let stack = '';
+        let reason = event.reason;
+        if (typeof reason === 'string') {
+            message = reason;
+        } else if (typeof reason === 'object') {//说明是一个错误对象
+            message = reason.message;
+            //at http://localhost:8080/:23:38
+            if (reason.stack) {
+                let matchResult = reason.stack.match(/at\s+(.+):(\d+):(\d+)/);
+                filename = matchResult[1];
+                line = matchResult[2];
+                column = matchResult[3];
+            }
+
+            stack = getLines(reason.stack);
+        }
+        tracker.send({
+            kind: 'stability',//监控指标的大类
+            type: 'error',//小类型 这是一个错误
+            errorType: 'promiseError',//JS执行错误
+            message,//报错信息
+            filename,//哪个文件报错了
+            position: `${line}:${column}`,
+            stack,
+            //body div#container div.content input
+            selector: lastEvent ? getSelector(lastEvent.path) : '' //代表最后一个操作的元素
+        });
+    }, true);
 }
